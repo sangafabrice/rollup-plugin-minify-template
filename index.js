c3e67af372fc8ddfeac75c20f2feaafef6c8af90
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-29 23:56:16 +0100

    build: use prettier to format code

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ type ExtensionArray = ReadonlyArray<Extension>;
 
 export type OptionExtension = ExtensionArray | Extension;
 
-const defaultExtensions: ExtensionArray = [ ".html", ".css" ];
+const defaultExtensions: ExtensionArray = [".html", ".css"];
 
-function globifyExtensions(extensions: OptionExtension): string {
+function globifyExtensions(
+    extensions: OptionExtension
+): string {
     extensions = [extensions].flat();
     if (!extensions.length) extensions = defaultExtensions;
-    return `**/*${extensions.join(",").replace(/^\.([^,]+,.+)/, ".{$1}").replace(/,\./g, ",")}`;
+    return `**/*${extensions
+        .join(",")
+        .replace(/^\.([^,]+,.+)/, ".{$1}")
+        .replace(/,\./g, ",")}`;
 }
 
 /**
@@ -26,15 +31,22 @@ function globifyExtensions(extensions: OptionExtension): string {
  * @param options.extensions - The extensions of source files to minify.
  * @returns The Rollup plugin object.
  */
-export default function minifyTemplate(options: { extensions: OptionExtension } = { extensions: defaultExtensions }): Plugin {
-    const EXTENSIONS_GLOB = globifyExtensions(options.extensions);
+export default function minifyTemplate(
+    options: { extensions: OptionExtension } = {
+        extensions: defaultExtensions
+    }
+): Plugin {
+    const EXTENSIONS_GLOB = globifyExtensions(
+        options.extensions
+    );
     return {
         name,
         version,
 
         /** Minifies source files. */
         transform(code, id) {
-            if (!matchesGlob(id, EXTENSIONS_GLOB)) return null;
+            if (!matchesGlob(id, EXTENSIONS_GLOB))
+                return null;
             return {
                 code: minify(extname(id), code),
                 map: { mappings: "" }
